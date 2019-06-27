0b4c0600e2f5484c2f739a5954fe54113e0cdb57
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2019-06-27 18:19:48 +0300

    Speedup tests by tuning timings

--- test/instance.lua
+++ test/instance.lua
@@ -16,12 +16,13 @@ console.listen(console_sock)
 log.info('Console started at %s', console_sock)
 
 membership = require('membership')
--- tune periods to speed up test
+-- Tune periods to speed up tests
+-- Supposing loopback roundtrip is about 0.1ms
 opts = require('membership.options')
-opts.PROTOCOL_PERIOD_SECONDS = 0.4
-opts.ACK_TIMEOUT_SECONDS = 0.2
-opts.ANTI_ENTROPY_PERIOD_SECONDS = 1.0
-opts.SUSPECT_TIMEOUT_SECONDS = 1.0
+opts.PROTOCOL_PERIOD_SECONDS = 0.050
+opts.ACK_TIMEOUT_SECONDS = 0.025
+opts.ANTI_ENTROPY_PERIOD_SECONDS = 0.100
+opts.SUSPECT_TIMEOUT_SECONDS = 0.100
 
 membership.init(hostname, tonumber(listen))
 _G.is_initialized = true
