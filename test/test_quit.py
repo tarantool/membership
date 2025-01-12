54041d6c7d6d6a0a5b8fd18646b69c244a582d57
Igor Zolotarev <yngvar.antonsson@gmail.com>
2025-01-12 19:25:06 +0300

    fix blanks

--- test/test_quit.py
+++ test/test_quit.py
@@ -20,6 +20,7 @@ def test_rejoin(servers, helpers):
     assert servers[13301].add_member('localhost:13302')
     helpers.wait_for(servers[13301].check_status, ['localhost:13302', 'alive'])
 
+
 def test_mark_left(servers, helpers):
     assert servers[13301].conn.eval('return membership.mark_left("localhost:13302")')[0]
     helpers.wait_for(servers[13301].check_status, ['localhost:13302', 'left'])
