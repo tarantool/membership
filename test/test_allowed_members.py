dd8f45db0b5b4544da330e4dd867f976b1ab4194
Igor Zolotarev <yngvar.antonsson@gmail.com>
2025-02-28 13:50:38 +0300

    Trying to fix test again

--- test/test_allowed_members.py
+++ test/test_allowed_members.py
@@ -9,9 +9,6 @@ def test(servers, helpers):
     servers[13303].kill()
     servers[13304].kill()
 
-    helpers.wait_for(servers[13301].check_status, ['localhost:13303', 'dead'])
-    helpers.wait_for(servers[13301].check_status, ['localhost:13304', 'dead'])
-
     servers[13301].conn.eval("""
         return membership.set_allowed_members({
             'localhost:13301', 'localhost:13302', 'localhost:13303',
