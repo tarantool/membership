b2d388cd696d802b245f730a30e0ec4604671b38
p0rtale <y.petryaev@vkteam.ru>
2025-05-28 13:08:42 +0300

    Remove python tests

--- test/test_dead_myself.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-hostname = "not-available"
-servers_list = [13301]
-
-
-def test(servers, helpers):
-    helpers.wait_for(servers[13301].check_status, ['not-available:13301', 'dead'])
